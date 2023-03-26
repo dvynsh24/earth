24d02da7e9b8cb81f14e7c4846c4a6c8f7a7c15c
dvynsh24 <dvynsh24@gmail.com>
2023-03-26 17:12:08 +0530

    added new class Test

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("hello world")
+class Test:
+    def __init__(self, test1: int, test2: float, test3: str) -> None:
+        self.test1: int = test1
+        self.test2: float = test2
+        self.test3: str = test3
+
+    def __repr__(self) -> str:
+        print(f"{self.test1}, {self.test2}, {self.test3}")
+
+
+test_obj1 = Test(1, 2.0, "3")
+print(test_obj1)
